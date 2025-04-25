2a54fd2550a0de4d321bbd0a26bccb081295894d
sagar72as <sagar72.as@gmail.com>
2025-04-25 15:32:57 +0000

    2nd

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈My app")
+st.title("🎈My Streamlit app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
