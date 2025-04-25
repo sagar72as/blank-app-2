6daecafbcdc8ab71e523b982aed9581f3f562738
sagar72as <sagar72.as@gmail.com>
2025-04-25 15:35:38 +0000

    2nd

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,4 @@ st.title("🎈My Streamlit app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+st.write("hey check this out")
